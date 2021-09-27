110870cb85b87088940cd7720d78acdf428bb7a0
BogdanLapshin <bogdanizizmaila@gmail.com>
2021-09-27 12:50:39 +0300

    MAG-32 Create Widget

--- app/design/frontend/BogdansTheme/new_theme/Magento_Theme/requirejs-config.js
+++ app/design/frontend/BogdansTheme/new_theme/Magento_Theme/requirejs-config.js
@@ -6,7 +6,8 @@ var config = {
             objectEditor: 'Magento_Theme/js/object-editor',
             uiCreator: 'Magento_Theme/js/ui-creator',
             uiXML: 'Magento_Theme/js/ui-xml',
-            dataPassing: 'Magento_Theme/js/data-passing'
+            dataPassing: 'Magento_Theme/js/data-passing',
+            jqueryWidget: 'Magento_Theme/js/jquery-widget'
         }
     },
     config: {
